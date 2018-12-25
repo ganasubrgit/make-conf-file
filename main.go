df90d760918aae81318ba40ce5759ecebe2297d0
hifly <hifly@hiflys-MacBook-Pro.local>
2018-12-25 10:59:50 -0600

    updated service

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	check(err)
 	defer f.Close()
 	f.WriteString("[Unit]\nDescription=Service demon for veld.py\n")
-	f.WriteString("\n[Service]\nUser=root\nWorkingDirectory=/etc/PitCrew\nExecStart=/usr/bin/python3 /etc/PitCrew/vel.py\nRestart=always")
+	f.WriteString("\n[Service]\nUser=root\nWorkingDirectory=/etc/PitCrew\nExecStart=/usr/bin/python2.7 /etc/PitCrew/vel.py\nRestart=always")
 	f.WriteString("\n\n[Install]\nWantedBy=multi-user.target\n\n")
 	os.Chmod(SRVFILE, 0644)
 	c := exec.Command("/bin/sh", "-c", "systemctl daemon-reload;systemctl start veld.service;systemctl enable veld.service")
