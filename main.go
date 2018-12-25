d987e3a2b2aff4f1f6efca1b3580df8012ba943b
hifly <hifly@hiflys-MacBook-Pro.local>
2018-12-25 17:43:57 -0600

    updated

--- main.go
+++ main.go
@@ -22,5 +22,5 @@ func main() {
 	os.Chmod(SRVFILE, 0644)
 	c := exec.Command("/bin/sh", "-c", "systemctl daemon-reload;systemctl start veld.service;systemctl enable veld.service")
 	c.Run()
-	println("System service installed successfully...")
+	println("Veld service installed successfully...")
 }
