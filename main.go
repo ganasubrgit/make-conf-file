ecf5937846729f37565efea7b3a1107d7b0ad8ff
hifly <hifly@hiflys-MacBook-Pro.local>
2018-12-25 10:50:29 -0600

    updated

--- main.go
+++ main.go
@@ -12,15 +12,14 @@ func check(e error) {
 }
 
 func main() {
-	SRVFILE := "/etc/systemd/system/example.service"
-	//SRVFILE := "/tmp/example.service" //tmp path for testing
+	SRVFILE := "/etc/systemd/system/veld.service"
 	f, err := os.Create(SRVFILE)
 	check(err)
 	defer f.Close()
-	f.WriteString("[Unit]\nDescription=Restarts example.py\n")
-	f.WriteString("\n[Service]\nUser=your username\nWorkingDirectory=/directory/of/script\nExecStart=/usr/bin/python3 /directory/of/script/example.py\nRestart=always")
+	f.WriteString("[Unit]\nDescription=Service demon for veld.py\n")
+	f.WriteString("\n[Service]\nUser=root\nWorkingDirectory=/etc/PitCrew\nExecStart=/usr/bin/python3 /etc/PitCrew/vel.py\nRestart=always")
 	f.WriteString("\n\n[Install]\nWantedBy=multi-user.target\n\n")
 	os.Chmod(SRVFILE, 0644)
-	c := exec.Command("/bin/sh", "-c", "systemctl daemon-reload;systemctl start example.service;systemctl enable example.service")
+	c := exec.Command("/bin/sh", "-c", "systemctl daemon-reload;systemctl start veld.service;systemctl enable veld.service")
 	c.Run()
 }
